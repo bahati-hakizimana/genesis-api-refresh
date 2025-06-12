4d57e32aadad05e048085dc6c089e7be9c59988d
bahati-hakizimana <hakizimanabahati2019@gmail.com>
2025-06-12 01:40:37 -0700

    feat:added Login, Logout, get profile data and api protection

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -6,10 +6,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class ApiController extends Controller
 {
-    //Register API(POST)
     public function register(Request $request) {
 
         $request ->validate([
@@ -33,21 +33,56 @@ class ApiController extends Controller
 
     }
 
-    // Login Api (POST)
-
     public function login(Request $request){
 
-    }
+        $request ->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
 
-    // profile Api (GET)
+        $user = User::where("email", $request->email)->first();
+
+        if(!empty($user)){
+
+            if(Hash::check($request->password, $user->password)){
+
+                $token = $user->createToken("myToken")->plainTextToken;
+                return response()->json([
+                    "status"=>true,
+                    "message" => "Login successfully",
+                    "token" => $token
+                ]);
+
+            }
+           return response()->json([
+            "status" => false,
+            "message" => "Invalid password"
+           ]);
+        }
+        return response()->json([
+            "status" =>false,
+            "message" => "Invalid user creadentials"
+        ]);
+
+    }
 
     public function profile(){
 
+        $data = auth()->user();
+        return response()->json([
+            "status" =>true,
+            "message" => "Profile data fetched successfully",
+            "data"=>$data
+        ]);
+
     }
-
-
-    //  Logout Api(Get)
     public function logout(){
+
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            "status"=>true,
+            "message" => "Logged out successfully"
+        ]);
 
     }
 }
