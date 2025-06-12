c0fc5ecaccc09caeb0182240d075c7310a5249ed
bahati-hakizimana <hakizimanabahati2019@gmail.com>
2025-06-11 23:52:20 -0700

    feat:added api routes and created Register api

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -4,11 +4,32 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class ApiController extends Controller
 {
     //Register API(POST)
     public function register(Request $request) {
+
+        $request ->validate([
+
+            "name" => "required",
+            "email" => "required|email|unique:users",
+            "password" => "required|confirmed"
+
+        ]);
+
+        User::create([
+            "name" => $request->name,
+            "email" => $request -> email,
+            "password" => Hash::make($request->password)
+        ]);
+
+       return response()->json([
+        "status" =>true,
+        "message" => "User registerd successful"
+       ]);
 
     }
 
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('register', [ApiController::class, "register"]);
+Route::post("login", [ApiController::class, "login"]);
+Route::group([
+    "middleware" => ["auth:sanctum"]
+], function(){
+
+    Route::get("profile", [ApiController::class, "profile"]);
+    Route::get("logout", [ApiController::class, "logout"]);
+
 });
